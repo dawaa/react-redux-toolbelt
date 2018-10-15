7be73a1942b227a44e21a27b3169250e6fd3590f
Alejandro Andersson <alejandro@thelad.io>
2018-10-15 07:43:44 +0200

    Export things individually from index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import withRedux from './react/utils/withRedux'
 import Container from './react/Container'
 
+export { withRedux }
+export { Container }
+
 export default {
   withRedux,
   Container,
